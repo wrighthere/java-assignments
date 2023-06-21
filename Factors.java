0dba1079c3a05e7ef48cdcba8729f04246dca338
Pavan <kedarpavanneerukonda@gmail.com>
2023-06-21 20:18:21 +0000

    sum and num of factors

--- Factors.java
+++ Factors.java
@@ -3,16 +3,24 @@ import java.util.Scanner;
 
 class Factors {
   public static void main(String[] args) {
+    int count = 0;
+    int sum = 0;
     System.out.println("Enter a number");
     Scanner sc = new Scanner(System.in);
     int num = sc.nextInt();
     for (int i = 1; i <= (int) Math.sqrt(num); i++) {
       if (num % i == 0) {
+        count += 1;
+        sum += i;
         System.out.print(i + " ");
         if (i != num / i) {
+          count += 1;
+          sum += (num / i);
           System.out.print(num / i + " ");
         }
       }
     }
+    System.out.print("\nnum of factors = " + count);
+    System.out.print("\nsum of factors = " + sum);
   }
 }
